a406cc7df777008e8d5a033b377d81f97dd7aa67
Jeffren <jeffrenmokamo726@gmail.com>
2025-03-08 15:19:41 +0100

    homepage

--- src/composants/Navbar.js
+++ src/composants/Navbar.js
@@ -2,17 +2,19 @@ import logo from '../assets/logos/logo.png'
 
 const Navbar = () => {
     return (
-        <div className="bg-white p-2">
+        <header className="bg-white p-2 menu">
             
             <div className="container d-flex align-items-center justify-content-between ">
                 <i class="bi bi-house-fill"></i>
 
-                <img src={logo} alt="logo" />
+                <a href="index">
+                    <img src={logo} alt="logo" />
+                </a>
 
                 <i class="bi bi-cart3"></i>
             </div>
 
-        </div>
+        </header>
     );
 };
 
